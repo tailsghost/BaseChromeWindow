79cfcac29f93197feebe94fe3da16715060fce8b
AndreyXP <andreyapple99@mail.ru>
2026-04-23 18:29:25 +0300

    Добавьте файлы проекта.

---
+++ Base.cs
@@ -0,0 +1,189 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Shell;
+
+namespace BaseChromeWindow;
+
+[ContentProperty(nameof(Body))]
+public class Base : Window
+{
+    public static readonly DependencyProperty HeaderProperty =
+        DependencyProperty.Register(
+            nameof(Header),
+            typeof(object),
+            typeof(Base),
+            new PropertyMetadata(null));
+
+    public object Header
+    {
+        get => GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+
+    public static readonly DependencyProperty BodyProperty =
+        DependencyProperty.Register(nameof(Body), typeof(object), typeof(Base), new PropertyMetadata(null));
+    public object Body
+    {
+        get => GetValue(BodyProperty);
+        set => SetValue(BodyProperty, value);
+    }
+
+    public ContentPresenter ContentArea { get; }
+
+    private (double left, double top) _prevPosition;
+    private bool _headerMouseDown;
+    private Point _mouseDownPoint;
+    private bool _isDragging;
+
+    public Base()
+    {
+        MinHeight = 200;
+        MinWidth = 200;
+        Title = "BaseWindow";
+        ResizeMode = ResizeMode.CanResizeWithGrip;
+        WindowStyle = WindowStyle.None;
+        SnapsToDevicePixels = true;
+        Width = 800;
+        Height = 450;
+
+        var chrome = new WindowChrome
+        {
+            CaptionHeight = 0,
+            CornerRadius = new CornerRadius(0),
+            GlassFrameThickness = new Thickness(0),
+            ResizeBorderThickness = new Thickness(8),
+            UseAeroCaptionButtons = false
+        };
+        WindowChrome.SetWindowChrome(this, chrome);
+
+        var root = new Grid();
+        root.RowDefinitions.Add(new RowDefinition { Height = new GridLength(40) });
+        root.RowDefinitions.Add(new RowDefinition());
+
+        var headerGrid = new Grid();
+        headerGrid.ColumnDefinitions.Add(new ColumnDefinition());
+        headerGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+
+        headerGrid.MinHeight = 40;
+
+        var headerControl = new ContentControl();
+        headerControl.SetBinding(
+            ContentControl.ContentProperty,
+            new Binding(nameof(Header)) { Source = this });
+        headerControl.MouseLeftButtonDown += Header_MouseLeftButtonDown;
+        headerControl.MouseMove += Header_MouseMove;
+        headerControl.MouseLeftButtonUp += Header_MouseLeftButtonUp;
+        Grid.SetColumn(headerControl, 0);
+        headerGrid.Children.Add(headerControl);
+
+        Grid.SetRow(headerGrid, 0);
+        root.Children.Add(headerGrid);
+
+        ContentArea = new ContentPresenter();
+        ContentArea.SetBinding(
+            ContentPresenter.ContentProperty,
+            new Binding(nameof(Body)) { Source = this });
+        Grid.SetRow(ContentArea, 1);
+        root.Children.Add(ContentArea);
+
+        Content = root;
+    }
+
+    #region Button Handlers
+
+    public virtual void MinimizeClick(object s, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    public virtual void MaximizeClick(object s, RoutedEventArgs e)
+    {
+        if (WindowState == WindowState.Normal)
+        {
+            _prevPosition = (Left, Top);
+            var mi = MonitorHelper.GetMonitorInfoForWindow(this);
+            var work = mi.rcWork;
+            Left = work.Left;
+            Top = work.Top;
+            MaxWidth = work.Right - work.Left + 8;
+            MaxHeight = work.Bottom - work.Top + 8;
+            WindowState = WindowState.Maximized;
+        }
+        else
+        {
+            MaxWidth = double.PositiveInfinity;
+            MaxHeight = double.PositiveInfinity;
+            WindowState = WindowState.Normal;
+            Left = _prevPosition.left;
+            Top = _prevPosition.top;
+        }
+    }
+
+    public virtual void CloseClick(object s, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    #endregion
+
+    #region Dragging Header
+
+    public virtual void Header_MouseLeftButtonDown(object s, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton != MouseButton.Left) return;
+
+        _headerMouseDown = true;
+        _isDragging = false;
+        _mouseDownPoint = e.GetPosition(this);
+
+        if (e.ClickCount == 2)
+        {
+            MaximizeClick(s, null);
+            _headerMouseDown = false;
+        }
+    }
+
+    protected virtual void Header_MouseMove(object s, MouseEventArgs e)
+    {
+        if (!_headerMouseDown || _isDragging || e.LeftButton != MouseButtonState.Pressed)
+            return;
+
+        var current = e.GetPosition(this);
+        const double threshold = 3;
+        if (Math.Abs(current.X - _mouseDownPoint.X) < threshold &&
+            Math.Abs(current.Y - _mouseDownPoint.Y) < threshold)
+            return;
+
+        _isDragging = true;
+
+        if (WindowState == WindowState.Maximized)
+        {
+            var relX = _mouseDownPoint.X / ActualWidth;
+            var screenPt = PointToScreen(_mouseDownPoint);
+
+            MaxWidth = double.PositiveInfinity;
+            MaxHeight = double.PositiveInfinity;
+            WindowState = WindowState.Normal;
+
+            Width = RestoreBounds.Width;
+            Height = RestoreBounds.Height;
+            Left = screenPt.X - Width * relX;
+            Top = screenPt.Y - _mouseDownPoint.Y;
+        }
+
+        DragMove();
+    }
+
+
+
+    protected virtual void Header_MouseLeftButtonUp(object s, MouseButtonEventArgs e)
+    {
+        _headerMouseDown = false;
+        _isDragging = false;
+    }
+
+    #endregion
+}
---
+++ MonitorHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace BaseChromeWindow;
+
+public static class MonitorHelper
+{
+
+    private const uint MONITOR_DEFAULTTONEAREST = 2;
+    [DllImport("user32.dll")]
+    private static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT { public int Left, Top, Right, Bottom; }
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MONITORINFO
+    {
+        public int cbSize;
+        public RECT rcMonitor;
+        public RECT rcWork;
+        public int dwFlags;
+    }
+
+    public static MONITORINFO GetMonitorInfoForWindow(Window window)
+    {
+        var hwnd = new WindowInteropHelper(window).Handle;
+        var hMon = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+        var mi = new MONITORINFO { cbSize = Marshal.SizeOf<MONITORINFO>() };
+        GetMonitorInfo(hMon, ref mi);
+        return mi;
+    }
+}
